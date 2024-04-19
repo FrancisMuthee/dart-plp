3f29875761d74a564f87679b4f7da6e362485f23
Francis <francisnjaramba2@gmail.com>
2024-04-19 09:15:05 +0300

    small changes

---
+++ calculator.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print('Simple calculator');
+
+  print('Enter Ist number');
+  
+  print('Enter 2nd number');
+  // double
+
+  
+}
---
+++ phind.dart
@@ -0,0 +1,11 @@
+// import 'package:function_tree/function_tree.dart';
+
+// double evaluateExpression(String exp) {
+//  return exp.interpret().toDouble();
+// }
+
+// void main() {
+//  String expression = "2 + 3 * 4"; // Example expression
+//  double result = evaluateExpression(expression);
+//  print("Result: $result");
+// }
